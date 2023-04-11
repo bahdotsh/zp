0f697bad050efad844480f1eae97c7dc45bd6116
bahdotsh <appu.yess@gmail.com>
2023-04-11 23:30:47 +0530

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use cli_clipboard::{ClipboardProvider, ClipboardContext};
+use cli_clipboard;
 use std::error::Error;
 use std::fs;
 
@@ -30,9 +30,7 @@ pub fn run(query: Query) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn cpy<'a>(contents: &'a str) -> Result<(), Box<dyn Error>> {
-    let mut ctx = ClipboardContext::new().unwrap();
-     
-    ctx.set_contents(contents.to_owned())
+    cli_clipboard::set_contents(contents.to_owned())
 }
 
 #[cfg(test)]

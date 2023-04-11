6449befc9095cfb7c4af56af9dc6a585190cc368
bahdotsh <appu.yess@gmail.com>
2023-04-11 15:39:13 +0530

    writing errors to stderror

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let query = Query::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing query: {err}");
+        eprintln!("Problem parsing query: {err}");
         process::exit(1);
     });
 
     if let Err(e) = cpy::run(query) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }

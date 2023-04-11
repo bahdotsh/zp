27169596707ba8992c56788179a0fc65e9e3ae87
bahdotsh <appu.yess@gmail.com>
2023-04-11 14:15:29 +0530

    added the basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use cli_clipboard;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let source = &args[1];
+
+    println!("Copying from: {}", source);
+
+    let contents = fs::read_to_string(source)
+        .expect("Should have been able to read the file");
+
+    cli_clipboard::set_contents(contents.to_owned()).unwrap();
+
 }
+

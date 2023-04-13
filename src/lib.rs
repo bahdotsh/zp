0be97e5b776769f2a2400fe01ab50816d6798cb8
Gokul <appu.yess@gmail.com>
2023-04-13 10:38:29 +0400

    Cross platform compatibility and description (#7)
    * moved to arboard
    
    * added description for zp usage
    
    * v0.2.0
    
    * test fixed

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-use cli_clipboard;
+use arboard::Clipboard;
 use std::error::Error;
 use std::fs;
+use std::process;
 
 #[derive(Debug)]
 pub struct Query {
@@ -26,11 +27,19 @@ pub fn run(query: Query) -> Result<(), Box<dyn Error>> {
     let contents =
         fs::read_to_string(query.source).expect("Should have been able to read the file");
 
-    cpy(&contents)
+    cpy(&contents);
+
+    Ok(())
 }
 
-pub fn cpy<'a>(contents: &'a str) -> Result<(), Box<dyn Error>> {
-    cli_clipboard::set_contents(contents.to_owned())
+pub fn cpy<'a>(contents: &'a str) {
+    let mut clipboard = Clipboard::new().unwrap();
+
+    clipboard.set_text(contents).unwrap_or_else(|err| {
+        eprintln!("Couldn't copy to clipboard: {}", err);
+        process::exit(1);
+    });
+
 }
 
 #[cfg(test)]
@@ -39,7 +48,8 @@ mod tests {
 
     #[test]
     fn one_result() {
+        let mut clipboard = Clipboard::new().unwrap();
         let _ = cpy("Hello, world!");
-        assert_eq!(cli_clipboard::get_contents().unwrap(), "Hello, world!");
+        assert_eq!(clipboard.get_text().unwrap(), "Hello, world!");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 use zp::Query;
 use std::env;
 use std::process;
+use clap::Parser;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+/// Copy file contents 
+#[derive(Parser)]
+#[command(
+    author = "Gokul <@bahdotsh>",
+    version = VERSION,
+    about = "Tool to copy contents from a file",
+
+)]
+struct Zp {
+    ///Name of the file to copy from.
+    file_name: String,
+}
 
 fn main() {
+    Zp::parse();
     let query = Query::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing query: {err}");
         process::exit(1);
